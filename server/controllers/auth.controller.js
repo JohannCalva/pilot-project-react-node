e2de8d0dfc7d56beb27d0bbb3c06665920407965
JohannCalva <johanncalvacadme@gmail.com>
2026-04-05 22:48:42 -0500

    feat: Login and Logout Logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -33,4 +33,35 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect Password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Hubo un error inesperado",
+    });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", { expires: new Date(0) });
+  return res.sendStatus(200);
+};
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 // Se definen las rutas y la funcion que ejecutaran
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
