df37d5ebac452c0dc95457949cfdf14f77f67e2e
JohannCalva <johanncalvacadme@gmail.com>
2026-04-05 21:58:41 -0500

    feat: Password Encrypt

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,15 +1,25 @@
 import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
 // Funciones que van a ejecutar las rutas
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
   try {
+    //Ejecuta bcrypt y su metodo hash para encriptar la contrasena
+    // El 10 indica las veces que se ejecutara el algoritmo
+    const passwordHash = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
     console.log(error);
     return res.status(400).json({
